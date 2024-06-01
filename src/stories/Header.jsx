772b085c49d32908d42fa5ead6ec2ff2b3734599
Second-FirstKiss <dhl125@soongsil.ac.kr>
2024-06-01 13:42:13 +0900

    재커밋

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -1,54 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from './IconButton';
+import styled from 'styled-components';
 
 const Header = ({ showIcon, text, backButton }) => {
-  const headerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between', // 좌우 여백을 최대한 벌리기 위해 space-between으로 설정
-    alignItems: 'flex-end', // 아이콘과 텍스트를 아래로 정렬하기 위해 추가
-    position: 'fixed', // 상단 고정
-    top: 0, // 화면 상단에 고정
-    width: '100%', // 화면 너비만큼 가로 폭 설정
-    height: '111px',
-    backgroundColor: 'white',
-    color: 'black',
-    fontSize: '21px',
-    padding: '10px 10px 20px 10px', // 좌우 padding 추가
-    zIndex: '1000', // 다른 요소들보다 위에 나타나도록 설정
-  };
-
-  const iconButtonStyle = {
-    position: 'absolute',
-    left: '20px', // 왼쪽에 고정
-    top: '50%',
-    transform: 'translateY(-50%)',
-  };
-
-  const centerContentStyle = {
-    position: 'absolute',
-    left: '50%',
-    transform: 'translateX(-50%)', // 가운데 정렬
-  };
-
   return (
-    <div style={headerStyle}>
+    <TopLayout bgColor="white">
       {backButton && (
-        <IconButton
-          src="/Icons/Icon_arrow.svg"
-          width="40px"
-          height="40px"
-          style={iconButtonStyle}
-        />
+        <IconButtonWrapper>
+          <IconButton
+            src="/Icons/Icon_arrow.svg"
+            width="40px"
+            height="40px"
+          />
+        </IconButtonWrapper>
       )}
-      <div style={centerContentStyle}>
+      <CenterContent>
         {showIcon ? (
           <img src="/Logo-color.svg" alt="로고" />
         ) : (
           <span>{text}</span>
         )}
-      </div>
-    </div>
+      </CenterContent>
+    </TopLayout>
   );
 };
 
@@ -60,8 +34,38 @@ Header.propTypes = {
 
 Header.defaultProps = {
   showIcon: false,
-  text: '',
+  text: '그룹 만들기',
   backButton: true,
 };
 
 export default Header;
+
+const TopLayout = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: ${(props) => props.bgColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 49px;
+  z-index: 1000;
+  padding: 10px 10px 20px 10px;
+`;
+
+const IconButtonWrapper = styled.div`
+  position: absolute;
+  left: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const CenterContent = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 21px;
+  font-weight: 600;
+  color: black;
+`;
--- src/stories/IconButton.stories.jsx
+++ src/stories/IconButton.stories.jsx
@@ -94,3 +94,42 @@ MyPage.args = {
   hoverBackgroundColor: 'rgba(0, 0, 0, 0.1)',
   disabledIcon: '/Icons/Icon_mypage_c.svg',
 };
+
+export const Expressions = () => {
+  const iconStyle = {
+    display: 'flex',
+    marginRight: '30px', // 아이콘 사이의 간격 조절
+  };
+
+  const icons = [
+    { src: '/FaceIcons/Blue20.svg', width: '48px', height: '67px', text: '퉤퉤', disabled: true, textsize: '10px', textcolor: 'black'},  
+    { src: '/FaceIcons/Blue40.svg', text: '맛있..나?', disabled: true},
+    { src: '/FaceIcons/Blue60.svg', text: '맛있다', disabled: true},
+    { src: '/FaceIcons/Blue80.svg', text: '맛도리', disabled: true},
+    { src: '/FaceIcons/Blue100.svg', text: '짱맛도리', disabled: true},
+    { src: '/FaceIcons/Gray20.svg', text: '퉤퉤', disabled: true},
+    { src: '/FaceIcons/Gray40.svg', text: '맛있..나?', disabled: true},
+    { src: '/FaceIcons/Gray60.svg', text: '맛있다', disabled: true},
+    { src: '/FaceIcons/Gray80.svg', text: '맛도리', disabled: true},
+    { src: '/FaceIcons/Gray100.svg', text: '짱맛도리', disabled: true},
+  ];
+
+  const topIcons = icons.slice(0, 5); // 상단 아이콘들
+  const bottomIcons = icons.slice(5); // 하단 아이콘들
+
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        {topIcons.map((icon, index) => (
+          <IconButton key={index} style={index === topIcons.length - 1 ? {} : iconStyle} {...icon} />
+        ))}
+      </div>
+      <div style={{ display: 'flex' }}>
+        {bottomIcons.map((icon, index) => (
+          <IconButton key={index + 5} style={index === bottomIcons.length - 1 ? {} : iconStyle} {...icon} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
