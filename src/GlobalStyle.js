e922bc12733c35116928b4521fc5d3a814159169
Pyeoun Yuna <78300392+Drizzle03@users.noreply.github.com>
2024-06-05 14:39:05 +0900

    Edit: Splash화면 로고 변경 및 폰트 적용

---
+++ src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import 'pretendard/dist/web/static/pretendard.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Pretendard', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
--- src/stories/OnBoardingPage.jsx
+++ src/stories/OnBoardingPage.jsx
@@ -2,12 +2,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Link 컴포넌트를 가져옴
 import { Button } from './Button'; // Button 컴포넌트를 가져옴
+import Logo from './assets/logo.png';
+import styled from 'styled-components';
+import GlobalStyle from '../GlobalStyle';
+
+let TextBox = styled.div`
+  color: #FFF;
+  font-size: 23px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.23px;
+`
 
 const OnboardingPage = () => {
   return (
     <div style={{ backgroundColor: '#495EF6', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <img src='../../Logo.svg' alt="맛있다 로고" style={{ width: '200px', marginBottom: '20px' }} />
-      {/* 프로필 설정 페이지로 이동하는 링크 */}
+      <GlobalStyle/>
+      <TextBox>성장형 헬스 기록 서비스</TextBox>
+      <img src={Logo} alt="맛있다 로고" style={{ width: '224px', marginBottom: '20px' }} />
       <Link to="/profile-setup">
         <Button label="시작하기" type='border'/>
       </Link>
