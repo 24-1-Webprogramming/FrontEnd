9fe567d0e1aedd4af93a43352d5069bb81550f49
Pyeoun Yuna <78300392+Drizzle03@users.noreply.github.com>
2024-06-06 09:56:06 +0900

    Feat: DDay Ios Style DatePicker

--- src/stories/Pages/Home/Home.jsx
+++ src/stories/Pages/Home/Home.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { DatePicker } from 'ios-style-picker';
-import '../../Component/ios-style-picker.css';
 
 const Home = () => {
     return (
         <div>
-            <DatePicker
-                infinite
-                onChange={(y, m, d) => {
-                    console.log(y, m, d);
-                  }}
-            />
+            Home
         </div>
     );
 };
--- src/stories/Pages/Home/entry/DDay.jsx
+++ src/stories/Pages/Home/entry/DDay.jsx
@@ -7,105 +7,110 @@ import { dday } from '../../data/data';
 import { DatePicker } from 'ios-style-picker';
 import '../../../Component/ios-style-picker.css';
 
+const AnswerButtons = ({ step, totalSteps, isButtonEnabled, handleNextStep }) => (
+  <AnswerButtonContainer>
+    <Button
+      onClick={handleNextStep}
+      disabled={!isButtonEnabled}  // 버튼 활성화/비활성화 상태에 따라
+      label={step === totalSteps - 1 ? '완료' : '다음'}
+      type="primary"
+      size="medium"
+      style={{ margin: '5px 0', width: '321px' }}
+    />
+  </AnswerButtonContainer>
+);
+
 const DDay = () => {
-    const [step, setStep] = useState(0); // 단계
-    const [responses, setResponses] = useState(Array(dday.length).fill(null)); // 답변 저장 배열
-    const [isButtonEnabled, setIsButtonEnabled] = useState(false); // 버튼 활성화 여부
-    const [currentInput, setCurrentInput] = useState(''); // 현재 입력값
+  const [step, setStep] = useState(0); // 단계
+  const [responses, setResponses] = useState(Array(dday.length).fill(null)); // 답변 저장 배열
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false); // 버튼 활성화 여부
+  const [currentInput, setCurrentInput] = useState(''); // 현재 입력값
 
-    const handleInputChange = (value) => {
-        setCurrentInput(value);
-        setIsButtonEnabled(value.trim() !== ''); // 입력값이 비어 있지 않으면 버튼 활성화
-    };
+  const handleInputChange = (value) => {
+      setCurrentInput(value);
+      setIsButtonEnabled(value.trim() !== ''); // 입력값이 비어 있지 않으면 버튼 활성화
+  };
 
-    const handleDateChange = (y, m, d) => {
-        const dateValue = `${y}-${m}-${d}`;
-        const newResponses = responses.map((response, index) => index === step ? dateValue : response);
-        setResponses(newResponses);
-        setCurrentInput(dateValue);
-        setIsButtonEnabled(true); // 날짜가 선택되면 버튼 활성화
-        localStorage.setItem('dday', JSON.stringify(newResponses));
-    };
+  const handleDateChange = (y, m, d) => {
+      const dateValue = `${y}-${m}-${d}`;
+      const newResponses = responses.map((response, index) => index === step ? dateValue : response);
+      setResponses(newResponses);
+      setCurrentInput(dateValue);
+      setIsButtonEnabled(true); // 날짜가 선택되면 버튼 활성화
+      localStorage.setItem('dday', JSON.stringify(newResponses));
+  };
 
-    const handleNextStep = () => {
-        const newResponses = responses.map((response, index) => index === step ? currentInput : response);
-        setResponses(newResponses);
-        localStorage.setItem('dday', JSON.stringify(newResponses));
+  const handleNextStep = () => {
+      const newResponses = responses.map((response, index) => index === step ? currentInput : response);
+      setResponses(newResponses);
+      localStorage.setItem('dday', JSON.stringify(newResponses));
 
-        if (step < dday.length - 1) {
-            setStep(step + 1);
-            setCurrentInput(''); // 입력값 초기화
-            setIsButtonEnabled(false); // 버튼 비활성화
-        } else {
-            console.log('dday complete:', newResponses);
-        }
-    };
+      if (step < dday.length - 1) {
+          setStep(step + 1);
+          setCurrentInput(''); // 입력값 초기화
+          setIsButtonEnabled(false); // 버튼 비활성화
+      } else {
+          console.log('dday complete:', newResponses);
+      }
+  };
 
-    return (
-        <>
-            <Header text="D-Day 등록" path="/home"/>
-            <Container>
-              <SurveyContent
-                  step={step}
-                  intro={dday[step].intro}
-                  question={dday[step].question}
-                  handleInputChange={handleInputChange}
-                  handleDateChange={handleDateChange}
-                  isButtonEnabled={isButtonEnabled}
-                  handleNextStep={handleNextStep}
-                  currentInput={currentInput}
-                  allowedCharsType={dday[step].allowedCharsType}
-              />
-            </Container>
-        </>
-    );
+  return (
+      <>
+          <Header text="D-Day 등록" path="/home"/>
+          <Container>
+            <SurveyContent
+                step={step}
+                intro={dday[step].intro}
+                question={dday[step].question}
+                handleInputChange={handleInputChange}
+                handleDateChange={handleDateChange}
+                isButtonEnabled={isButtonEnabled}
+                handleNextStep={handleNextStep}
+                currentInput={currentInput}
+                allowedCharsType={dday[step].allowedCharsType}
+            />
+          </Container>
+      </>
+  );
 };
 
 const SurveyContent = ({
-    step, intro, question, allowedCharsType, handleInputChange, handleDateChange, currentInput, isButtonEnabled, handleNextStep
-  }) => {
-    return (
-      <>
-        <ContainerTop>
-          <StepText>{step + 1}/{dday.length}</StepText>
-          <TextBox>
-            <h2 style={{ color: '#000', textAlign: 'left', marginBottom: '5px' }}>{intro}</h2>
-            <h2 style={{ color: '#495EF6', textAlign: 'left', marginTop: '5px' }}>{question}</h2>
-          </TextBox>
-          {step === 1 ? (
-            <DatePicker
-              infinite
-              onChange={handleDateChange}
-            />
-          ) : (
-            <TextField 
-              onChange={(e) => handleInputChange(e.target.value)}
-              value={currentInput}
-              allowedCharsType={allowedCharsType}
-              placeholder=""
-              customText={dday[step].unit}
-              width="321px"
-            />
-          )}
-        </ContainerTop>
+  step, intro, question, allowedCharsType, handleInputChange, handleDateChange, currentInput, isButtonEnabled, handleNextStep
+}) => {
+  return (
+    <>
+      <ContainerTop>
+        <StepText>{step + 1}/{dday.length}</StepText>
+        <TextBox>
+          <h2 style={{ color: '#000', textAlign: 'left', marginBottom: '5px' }}>{intro}</h2>
+          <h2 style={{ color: '#495EF6', textAlign: 'left', marginTop: '5px' }}>{question}</h2>
+        </TextBox>
+        {step === 1 ? (
+          <DatePicker
+            onChange={handleDateChange}
+          />
+        ) : (
+          <TextField 
+            onChange={(e) => handleInputChange(e.target.value)}
+            value={currentInput}
+            allowedCharsType={allowedCharsType}
+            placeholder=""
+            customText={dday[step].unit}
+            width="321px"
+          />
+        )}
+      </ContainerTop>
 
-        <AnswerButtons isButtonEnabled={isButtonEnabled || step === 1} handleNextStep={handleNextStep} />
-      </>
-    );
+      <AnswerButtons 
+        step={step}
+        totalSteps={dday.length}
+        isButtonEnabled={isButtonEnabled || step === 1} 
+        handleNextStep={handleNextStep} 
+      />
+    </>
+  );
 };
 
-const AnswerButtons = ({ isButtonEnabled, handleNextStep }) => (
-    <AnswerButtonContainer>
-      <Button
-        onClick={handleNextStep}
-        disabled={!isButtonEnabled}  // 버튼 활성화/비활성화 상태에 따라
-        label={isButtonEnabled && dday.length - 1 ? '완료' : '다음'}
-        type="primary"
-        size="medium"
-        style={{ margin: '5px 0', width: '321px' }}
-      />
-    </AnswerButtonContainer>
-);
 
 const StepText = styled.div`
   color: var(--Base-Gray-700, #3F3F45);
