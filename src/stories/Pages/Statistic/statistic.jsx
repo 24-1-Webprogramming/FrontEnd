780fe0cd07222405636f28cada085834a1608c7a
Pyeoun Yuna <78300392+Drizzle03@users.noreply.github.com>
2024-06-10 08:22:18 +0900

    Temp: 통계페이지 캘린더+그래프

--- src/stories/Pages/Statistic/statistic.jsx
+++ src/stories/Pages/Statistic/statistic.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import Header from '../../Component/Header';
 import NavBar from '../../Component/NavBar';
+import CalendarSlides from '../../Component/CalendarSlides';
+import styled from 'styled-components';
+import Graph from '../../Component/Graph';
+
+const data = [10, 20, 15, 250, 30, 200, 10];
 
 const DashBoard = () => {
     return (
         <>
         <Header showIcon={true} text="홈" backButton={false} />
-            <div style={{ paddingTop: '60px', paddingBottom: '50px', minHeight: 'calc(100vh - 110px)', overflowY: 'auto' }}> {/* Header와 NavBar의 높이만큼 빼주기 위해 calc() 함수 사용 */}
-                <p>DashBoard 페이지</p>
-            </div>
+            <Container> 
+                <Graph data={data}
+                selectedIndex={6}/>
+                <CalendarSlides /> 
+            </Container>
         <NavBar activeState="Statistic" />
-    </>
+        </>
     );
 };
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+    margin-top: 130px;
+`
 
 export default DashBoard;
